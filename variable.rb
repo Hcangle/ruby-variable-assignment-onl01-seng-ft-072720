2fe602a177f0947bb5e301afc939c36e236f9384
Hannah Angle <hannahcangle@gmail.com>
2020-07-28 17:16:18 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,2 +1,5 @@
- # Define a local variable called 'greeting',
+authenticate_or_request_with_http_basic
+# Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
+
+greeting = "Hello World"
